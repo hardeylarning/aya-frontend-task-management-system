98b5a5421fdecba21649d306befc35bc830784ab
hardeylarning <hardeylarning@gmail.com>
2023-02-01 14:50:35 +0100

    user service, auth service, auth guard added

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,10 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../model/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+}
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,60 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../model/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private BASE_URL = 'http://localhost:5000/api/v1/users'
+
+  private message!: string 
+
+
+  constructor(private http: HttpClient) { }
+
+  setMessage(message: string) {
+    this.message = message
+  }
+
+  getMessage() {
+    return this.message
+  }
+
+  getUser(id: string) {
+    return this.http.get<User>(this.BASE_URL+'/'+id)
+  }
+
+  getUsers() {
+    return this.http.get<User[]>(this.BASE_URL)
+  }
+
+  addUser(user: User) {
+    return this.http.post<User>(this.BASE_URL +'/add', user)
+  }
+
+  updateUser(id:string, user: User) {
+    return this.http.put<User>(this.BASE_URL+'/'+id, user)
+  }
+
+  deleteUser(id:string) {
+    return this.http.delete<void>(this.BASE_URL+'/'+id)
+  }
+
+  login(email: string, password: string) {
+    const requestHeader = {
+      headers: new HttpHeaders(
+       { "No-Auth": "True"}
+      )
+    }
+    return this.http.post(`${this.BASE_URL}/login`, { email, password }, requestHeader)
+  }
+
+  isLoggedIn() {
+    return localStorage.getItem('token') != null
+  }
+
+  getToken() {
+    return localStorage.getItem('token') || ''
+  }
+}
